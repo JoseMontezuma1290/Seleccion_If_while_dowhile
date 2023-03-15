66b8be37ccd63109bffe4cda1629240524a96887
Jose Montezuma <josemont7902@gmail.com>
2023-03-14 18:38:37 -0600

    Firts Version

---
+++ Seleccion_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+main(){
+/*	int i=0;
+	while(i<5){
+		cout<<"While: <<i<<endl;
+		i++;
+		
+	}*/
+	
+	//int ii=5;
+	
+/*	char respuesta;
+	do{
+		cout<<"Desea ingresar otro valor (s/n): ";
+		cin>>respuesta;
+		
+
+		
+		}while(respuesta=='s'|| respuesta=='S');
+		*/
+		
+	char respuesta='s';
+	
+	while(respuesta=='s'|| respuesta=='S');{
+		
+		cout<<"Desea ingresar otro valor (s/n): ";
+		cin>>respuesta;		
+		}
+	
+	system("pause");
+	
+}
